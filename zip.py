c80e8aaf99e1592c8b557641e872be42fca7ef6c
Buffett Liu <buffettliu@ip71-193.vpnip.cc.ntu.edu.tw>
2023-09-13 15:20:05 +0800

    0913 Zip final results

--- zip.py
+++ zip.py
@@ -1,12 +1,11 @@
 '''
-Use Elastic Nets to filter some columns and then use Zero-inflated Model
+Use Elastic Nets to filter some columns and then use Zero-inflated Model for Y 0-100
 Ref from https://timeseriesreasoning.com/contents/zero-inflated-poisson-regression-model/
 '''
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import statsmodels.api as sm
-import random
 import warnings
 
 from tqdm import tqdm
@@ -21,7 +20,7 @@ warnings.filterwarnings("ignore")
 plotting = True
 
 # Read data
-data = pd.read_csv('../Data/data_0802.csv', encoding='utf-8')
+data = pd.read_csv('data_0802.csv', encoding='utf-8')
 data = data.drop(data.columns[0], axis=1)
 X_columns = [c for c in data.columns if c != '已填寫問卷數量']
 
@@ -32,47 +31,39 @@ X_data = data[X_columns]
 scaler = MinMaxScaler() # StandardScaler()
 X_data_scaled = pd.DataFrame(scaler.fit_transform(X_data), columns=X_data.columns)
 
-
-# Grid Search for Elastic Nets
-param_grid = {
-    'alpha': [0.00005, 0.0001, 0.0005, 0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1.0],
-    'l1_ratio': [0.1, 0.5, 0.8, 0.9, 1]
-} 
-
-elastic_net = ElasticNet()
-grid_search = GridSearchCV(estimator=elastic_net, param_grid=param_grid, cv=5)
-grid_search.fit(X_data_scaled, Y_data)
-print("Best Parameters:", grid_search.best_params_)
-print("Best Score:", grid_search.best_score_)
-
-
 # Elastic Net model for feature selection
-enet = ElasticNet(alpha=grid_search.best_params_['alpha'], l1_ratio=grid_search.best_params_['l1_ratio'], random_state=42)
+enet = ElasticNet(alpha=0.01, l1_ratio=0.1, random_state=42)
 enet.fit(X_data_scaled, Y_data)
 
-# Get selected features based on non-zero coefficients
+# Get selected features based on non-zero coefficients --> X_enet, Y_data
 selected_features = X_data_scaled.columns[enet.coef_ != 0].tolist()
+X_enet = X_data_scaled[selected_features]
 
 
-# Randomly select some features
-sum_list, llf_list = [], []
-for _ in tqdm(range(100)):
-    l = len(selected_features)
-    random_select = random.sample(selected_features, random.randint(l // 4, l // 2))
-    X_enet = X_data_scaled[random_select]
-    X_train, X_test, y_train, y_test = train_test_split(X_enet, Y_data, test_size=0.2, random_state=42)
+
+
+# Train test split
+X_train, X_test, y_train, y_test = train_test_split(X_enet, Y_data, test_size=0.2, random_state=42)
+
+# Top performance
+top_perf = pd.read_csv('seq_sel_top_10.csv', encoding='utf-8')
+summary_list, feat_sel_list = [], []
+for i, j in tqdm(top_perf.iterrows()):
+    sel_feat = j[1][1:-1].split(', ')
+    sel_feat = [int(i) for i in sel_feat]
+    X_subset = X_train.iloc[:, sel_feat]
 
     # ZIP training, endog and exog with actual data and explanatory variables.
-    zip_training_results = sm.ZeroInflatedPoisson(endog=y_train, exog=X_train, exog_infl=X_train, 
-                                                  inflation='logit').fit(maxiter=100) # Try to make it converge
+    zip_training_results = sm.ZeroInflatedPoisson(endog=y_train, exog=X_subset, exog_infl=X_subset, 
+                                                    inflation='logit').fit(maxiter=1000) # Try to make it converge
+    smy = zip_training_results.summary()
     
+    # Evaluate by p-values
+    p_value = zip_training_results.pvalues.tolist()
+    exog_names = zip_training_results.model.exog_names
     
-    # print(f"Selected {len(random_select)} out of {len(selected_features)} features: {random_select}")
-    sum_list.append(zip_training_results.summary())
-    llf_list.append(zip_training_results.llf)
+    feats = [e_name for e_name, pv in zip(exog_names, p_value) if pv < 0.05]
+    feat_sel_list.append(feats)
 
-
-min_id = llf_list.index(max(llf_list))
-print(sum_list[min_id])
-
-
+print(feat_sel_list)
+    
